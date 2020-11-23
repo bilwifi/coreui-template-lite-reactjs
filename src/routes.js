7e0e28ff132c5d91c2f1c2a756631c85cdb43a83
Peniel <dialundamapeniel@gmail.com>
2020-11-23 14:55:34 +0100

    integration dashoboard

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'));
+const Dashboard = React.lazy(() => import('./views/dashboard/'));
 
 
 const routes = [
