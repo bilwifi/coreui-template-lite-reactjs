392d3e7d444ace18eec1a0da499bd8f265ad6727
Peniel <dialundamapeniel@gmail.com>
2020-11-23 15:08:52 +0100

    customisation footer

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -5,12 +5,12 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        {/* <a href="https://orange.cd" target="_blank" rel="noopener noreferrer">Orange</a> */}
+        <span className="ml-1">&copy; 2020 Orange RDC.</span>
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">CoreUI for React</a>
+        <a href="http://kinshasadigital.academy/" target="_blank" rel="noopener noreferrer">Kinshasa Digital Academy</a>
       </div>
     </CFooter>
   )
